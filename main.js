8f1d73143c77372c53682aa42d3261fb85972d82
ruthjoy76 <ruthjoy76@gmail.com>
2023-04-25 17:30:47 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import './style.css'
+
+const quoteText = document.querySelector(".random-quote"),
+quoteBtn = document.querySelector(".btn"),
+authorName = document.querySelector(".name");
+
+
+function randomQuote(){
+    quoteBtn.classList.add("search");
+    quoteBtn.innerText = "";
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(data => {
+        quoteText.innerText = data.content;
+        authorName.innerText = data.author;
+        quoteBtn.classList.remove("search");
+        quoteBtn.innerText = "";
+    });
+}
+
+
+
+quoteBtn.addEventListener("click", randomQuote);
